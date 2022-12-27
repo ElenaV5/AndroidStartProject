6679ea1c572779ea7adae14cbfe96406899eb605
ElenaVN <elenavnev@gmail.com>
2022-12-27 14:14:56 +0500

    Added Java screen configuration example with Constraint Layout

--- app/src/main/java/com/example/androidstartproject/MainActivity.java
+++ app/src/main/java/com/example/androidstartproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidstartproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,11 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+
         //создание нововго экрана без xml:
-        TextView textView = new TextView(this); //
+        /*TextView textView = new TextView(this); //
         textView.setText("Hello Android!");
         //textView.setText("Hello my new Android!"); //этот текст заменит предыдущий
         textView.setTextSize(22); //устанавливает размер текста
-        setContentView(textView); //выводит текст
+        setContentView(textView); //выводит текст*/
+
+        //привязка по направляющим
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+
+        TextView textView = new TextView(this);
+        textView.setText("Hello ANDROID!");
+        textView.setTextSize(26);
+
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID; //привязка к левому краю
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID; // привязка к верхнему краю
+        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID; // привязка к правому краю - текст будет посередине
+
+        textView.setLayoutParams(layoutParams);
+
+        constraintLayout.addView(textView);
+
+        setContentView(constraintLayout);
+
+
     }
 }
