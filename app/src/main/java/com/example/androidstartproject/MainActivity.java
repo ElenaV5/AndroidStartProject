1fceeb347a0920a4ab8b89e210a7faa348938cd1
ElenaVN <elenavnev@gmail.com>
2022-12-27 14:02:53 +0500

    Added Java screen configuration example

--- app/src/main/java/com/example/androidstartproject/MainActivity.java
+++ app/src/main/java/com/example/androidstartproject/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.androidstartproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        //создание нововго экрана без xml:
+        TextView textView = new TextView(this); //
+        textView.setText("Hello Android!");
+        //textView.setText("Hello my new Android!"); //этот текст заменит предыдущий
+        textView.setTextSize(22); //устанавливает размер текста
+        setContentView(textView); //выводит текст
     }
 }
