794c19bb4db25b33c6f3bc9d55b3a194e26b6a06
ElenaVN <elenavnev@gmail.com>
2022-12-30 11:48:08 +0500

    Added Go to InfActivity and Simple open Activity

--- app/src/main/java/com/example/androidstartproject/MainActivity.java
+++ app/src/main/java/com/example/androidstartproject/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -61,6 +62,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         et = (EditText) findViewById(R.id.et);
         tv = (TextView) findViewById(R.id.tv);
+
+        Button btn1 = (Button) findViewById(R.id.button);
+        Button btn2 = (Button) findViewById(R.id.button2);
+        Button btn3 = (Button) findViewById(R.id.button3);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
 
     }
 
--- app/src/main/java/com/example/androidstartproject/MainActivity2.java
+++ app/src/main/java/com/example/androidstartproject/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.androidstartproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        Button btn = (Button) findViewById(R.id.backB);
+
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
